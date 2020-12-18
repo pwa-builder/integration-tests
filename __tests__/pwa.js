4230af2bc5191ae4eb0eb5da79ec3c4a0e39ef22
Justin Willis <jgw9617@gmail.com>
2020-12-18 12:36:23 -0800

    chore(): move to playwright done

--- __tests__/pwa.js
+++ __tests__/pwa.js
@@ -1,77 +1,52 @@
 const timeout = 120000;
 
-require('expect-puppeteer');
+const { chromium } = require('playwright');
 const edgePaths = require("edge-paths");
 
 const EDGE_PATH = edgePaths.getEdgePath();
+
+const testURLs = [
+  'https://webboard.app',
+  'https://urzagatherer.app',
+  'https://app.starbucks.com',
+  'https://m.aliexpress.com/',
+  'https://pinterest.com',
+  'https://twitter.com'
+]
 
 describe(
   'Run URLs that are a PWA',
   () => {
     let page;
-    
+    let browser;
+
     beforeAll(async () => {
-      const browser = await chromium.launch({
+      browser = await chromium.launch({
         executablePath: EDGE_PATH
       });
-      const page = await browser.newPage();
-      
+    });
+
+    beforeEach(async () => {
+      page = await browser.newPage();
       await page.goto('https://www.pwabuilder.com/');
+    });
+
+    afterEach(async () => {
+      await page.close();
     })
 
     afterAll(async () => {
-      await page.close()
+      await browser.close()
     });
 
-    it('should load https://applescrusher.azurewebsites.net/ and validate as a PWA', async () => {
-      await page.type('#getStartedInput', 'https://applescrusher.azurewebsites.net/');
-      await expect(page).toClick('button', { text: 'Start' });
+    testURLs.map((url) => {
+      it(`should load ${url} and validate as a PWA`, async () => {
+        await page.fill('#getStartedInput', url);
+        await page.click('button#getStartedButton');
 
-      await expect(page).toMatchElement('#attachSection', { timeout: timeout });
-    }, timeout);
-
-    it('should load https://urzagatherer.app and validate as a PWA', async () => {
-      await page.type('#getStartedInput', 'https://urzagatherer.app');
-      await expect(page).toClick('button', { text: 'Start' });
-
-      await expect(page).toMatchElement('#attachSection', { timeout: timeout });
-    }, timeout);
-
-    it('should load https://app.starbucks.com and validate as a PWA', async () => {
-      await page.type('#getStartedInput', 'https://app.starbucks.com');
-      await expect(page).toClick('button', { text: 'Start' });
-
-      await expect(page).toMatchElement('#attachSection', { timeout: timeout });
-    }, timeout);
-
-    it('should load https://m.aliexpress.com/ and validate as a PWA', async () => {
-      await page.type('#getStartedInput', 'https://m.aliexpress.com/');
-      await expect(page).toClick('button', { text: 'Start' });
-
-      await expect(page).toMatchElement('#attachSection', { timeout: timeout });
-    }, timeout);
-
-    it('should load https://m.alibaba.com and validate as a PWA', async () => {
-      await page.type('#getStartedInput', 'https://m.alibaba.com');
-      await expect(page).toClick('button', { text: 'Start' });
-
-      await expect(page).toMatchElement('#attachSection', { timeout: timeout });
-    }, timeout);
-
-    it('should load https://twitter.com and validate as a PWA', async () => {
-      await page.type('#getStartedInput', 'https://twitter.com');
-      await expect(page).toClick('button', { text: 'Start' });
-
-      await expect(page).toMatchElement('#attachSection', { timeout: timeout });
-    }, timeout);
-
-    it('should load https://pinterest.com and validate as a PWA', async () => {
-      await page.type('#getStartedInput', 'https://pinterest.com');
-      await expect(page).toClick('button', { text: 'Start' });
-
-      await expect(page).toMatchElement('#attachSection', { timeout: timeout });
-    }, timeout);
-
+        await expect(page).toHaveSelector('#attachSection', { timeout: timeout });
+      }, timeout);
+    });
   },
   timeout
 )
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   preset: 'jest-playwright-preset',
+  "setupFilesAfterEnv": ["expect-playwright"]
 }
